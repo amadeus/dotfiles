4583f15190f8cadab3ab5184e61ab660b7d156ab
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-03-21 15:49:14 -0700

    misc: tweak .lvimrc.lua file

--- .lvimrc.lua
+++ .lvimrc.lua
@@ -5,5 +5,12 @@ end
 
 vim.g.localvimrc_sourced_once = 1
 vim.g.startify_disable_at_vimenter = 1
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+-- ALEFix Config
+vim.g.ale_fixers = {}
+vim.g.ale_fix_on_save = 1
 
 print("dotfiles repo .lvimrc has been sourced")
