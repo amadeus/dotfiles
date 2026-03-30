30df6d7d3c25ba05f5d2e04fcfcc7eb542170f48
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2026-03-30 16:37:50 -0700

    fish: trying to make prwt a bit smarter...

--- private_dot_config/private_fish/functions/prwt.fish
+++ private_dot_config/private_fish/functions/prwt.fish
@@ -25,11 +25,43 @@ function prwt --description "Check out a PR into a sibling worktree"
         return 1
     end
 
+    set pr_info (command gh pr view $pr --json headRefName,headRepository,headRepositoryOwner --jq '[.headRefName, .headRepositoryOwner.login, .headRepository.url] | @tsv')
+    or return $status
+
+    set pr_head_ref $pr_info[1]
+    set pr_head_owner $pr_info[2]
+    set pr_head_repo_url $pr_info[3]
+
+    set remote_name (command git -C "$root" remote | while read -l remote
+        set remote_url (command git -C "$root" remote get-url "$remote" 2>/dev/null)
+        if test "$remote_url" = "$pr_head_repo_url"
+            echo $remote
+            break
+        end
+    end)
+
+    if test -z "$remote_name"
+        set remote_name (string lower -- "$pr_head_owner")
+        if command git -C "$root" remote get-url "$remote_name" >/dev/null 2>/dev/null
+            set remote_name "$remote_name-pr"
+        end
+
+        command git -C "$root" remote add "$remote_name" "$pr_head_repo_url"
+        or return $status
+    end
+
+    command git -C "$root" fetch "$remote_name" "$pr_head_ref"
+    or return $status
+
     command gh pr checkout $pr -b $branch
+    or return $status
+
+    command git -C "$root" branch --set-upstream-to="$remote_name/$pr_head_ref" "$branch"
     or return $status
 
     command git -C "$root" checkout main
     or return $status
 
     command git -C "$root" worktree add "$worktree_path" $branch
+    or return $status
 end
